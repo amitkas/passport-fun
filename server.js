69cff16c8eae4ee1d12ffd2b0c2606afcfe79d68
amitkas <amit.kashtan@gmail.com>
2017-07-11 13:30:12 +0300

    login done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var passport = require('passport');
 var bodyParser = require('body-parser');
 var LocalStrategy = require('passport-local').Strategy;
-var session = require('express-session')
+var expressSession = require('express-session')
 
 
 app.use(express.static('public'));
@@ -17,10 +17,17 @@ app.use(passport.session());
 passport.serializeUser(function (user, done) {
   done(null, user.username);
 });
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
-app.get('/success', function(req, res) {
-  res.send("Hey, hello from the server!");
-})
+app.get('/success', function (req, res){
+  if (req.isAuthenticated()) {
+    res.send('Hey, ' + req.user + ', hello from the server!');
+  } else {
+    res.redirect('/login');
+  }
+});
 
 app.get('/login', function(req, res) {
   res.sendFile(__dirname + '/public/login.html');
@@ -30,6 +37,11 @@ app.post('/login', passport.authenticate('local', {
   successRedirect: '/success',
   failureRedirect: '/login',
 }));
+
+app.get('/logout', function (req, res) {
+  req.logout();
+  res.send('Logged out!');
+});
 
 passport.use(new LocalStrategy(function(username, password, done) {
   if ((username === "john") && (password === "password")) {
