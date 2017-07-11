8a52609ffc47ec4f08c6cbb4fc1e63d9db5f68d7
amitkas <amit.kashtan@gmail.com>
2017-07-11 12:10:59 +0300

    added session middleware

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ var app = express();
 var passport = require('passport');
 var bodyParser = require('body-parser');
 var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session')
 
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(expressSession({ secret: 'thisIsASecret', resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
+app.use(passport.session());
 
+passport.serializeUser(function (user, done) {
+  done(null, user.username);
+});
 
 app.get('/success', function(req, res) {
   res.send("Hey, hello from the server!");
@@ -19,10 +25,10 @@ app.get('/success', function(req, res) {
 app.get('/login', function(req, res) {
   res.sendFile(__dirname + '/public/login.html');
 });
+
 app.post('/login', passport.authenticate('local', {
   successRedirect: '/success',
   failureRedirect: '/login',
-  session: false
 }));
 
 passport.use(new LocalStrategy(function(username, password, done) {
