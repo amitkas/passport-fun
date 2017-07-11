0ed1b28a599e765bea374eb02a668a327146526f
amitkas <amit.kashtan@gmail.com>
2017-07-11 11:47:40 +0300

    server-req-client-form

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ var app = express();
 var passport = require('passport');
 var bodyParser = require('body-parser');
 
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
+
+
+app.get('/success', function(req, res) {
+  res.send("Hey, hello from the server!");
+})
+
+app.get('/login', function(req, res) {
+  res.sendFile(__dirname + '/public/login.html');
+});
+
 
 app.listen(8000, function() {
   console.log("Ready for some authentication action");
